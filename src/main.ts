81cd34c170f59868173b7b694591f0b1763895af
AlexDroomer <alexdroomer@gmail.com>
2023-08-01 17:54:12 +0300

    Project Update
    - Added Vite
    - Replace reset.css to normalize.css
    - Replace native js to alpine.js lib
    - Added deploy action to github pages
    - Added Typescript

---
+++ src/main.ts
@@ -0,0 +1,47 @@
+import 'normalize.css';
+import './style.css';
+import Alpine, { AlpineComponent } from 'alpinejs';
+
+window.Alpine = Alpine;
+
+type FeaturesStore = AlpineComponent<{
+  on: boolean;
+  categories: { name: string; link: string }[];
+  handleHeader: () => void;
+  toggleMenu: () => void;
+  handleSmoothScroll: (link: string) => void;
+}>;
+
+const featuresStore = {
+  on: false,
+  categories: [
+    { name: 'Articles', link: '#articles' },
+    { name: 'Locations', link: '#locations' },
+    { name: 'Videos', link: '#videos' },
+    { name: 'Sign In', link: '#sign-in' },
+  ],
+  handleHeader() {
+    const header = document.querySelector<HTMLElement>('.header')!;
+    if (window.scrollY > 125) {
+      header.classList.add('header_active');
+    }
+    if (window.scrollY < 125) {
+      header.classList.remove('header_active');
+    }
+  },
+
+  toggleMenu(this: FeaturesStore) {
+    this.on = !this.on;
+  },
+  handleSmoothScroll(this: FeaturesStore, link: string) {
+    if (window.innerWidth <= 767) {
+      this.toggleMenu();
+    }
+    const element = document.querySelector<HTMLElement>(link)!;
+    element.scrollIntoView({ behavior: 'smooth' });
+  },
+};
+
+Alpine.store('features', featuresStore);
+
+Alpine.start();
