53be7ed76449466c777cb3c7de6ae986c6628e64
AlexDroomer <alexdroomer@gmail.com>
2023-08-04 22:12:45 +0300

    upgrade performance and core web vitals

--- src/main.ts
+++ src/main.ts
@@ -50,15 +50,72 @@ const featuresStore = {
     }
   },
   openModal() {
-    const dialogElement = document.querySelector<HTMLDialogElement>('#sendRequestDialog')!
-    dialogElement.showModal()
+    const dialogElement =
+      document.querySelector<HTMLDialogElement>('#sendRequestDialog')!;
+    dialogElement.showModal();
   },
   closeModal() {
-    const dialogElement = document.querySelector<HTMLDialogElement>('#sendRequestDialog')!
-    dialogElement.close()
-  }
+    const dialogElement =
+      document.querySelector<HTMLDialogElement>('#sendRequestDialog')!;
+    dialogElement.close();
+  },
 };
 
 Alpine.store('features', featuresStore);
 
 Alpine.start();
+
+function findVideos() {
+  let videos: NodeListOf<HTMLVideoElement> =
+    document.querySelectorAll('.video');
+
+  for (let i = 0; i < videos.length; i++) {
+    setupVideo(videos[i]);
+  }
+}
+
+function setupVideo(video: HTMLVideoElement) {
+  let link: HTMLLinkElement = video.querySelector('.video__link')!;
+  let media: HTMLImageElement = video.querySelector('.video__media')!;
+  let button: HTMLButtonElement = video.querySelector('.video__button')!;
+  let id = parseMediaURL(media);
+
+  video.addEventListener('click', () => {
+    let iframe = createIframe(id);
+
+    link.remove();
+    button.remove();
+    video.appendChild(iframe);
+  });
+
+  link.removeAttribute('href');
+  video.classList.add('video--enabled');
+}
+
+function parseMediaURL(media: HTMLImageElement) {
+  let regexp =
+    /https:\/\/i\.ytimg\.com\/vi\/([a-zA-Z0-9_-]+)\/maxresdefault\.jpg/i;
+  let url = media.src;
+  let match = url.match(regexp)!;
+
+  return match[1];
+}
+
+function createIframe(id: string) {
+  let iframe = document.createElement('iframe');
+
+  iframe.setAttribute('allowfullscreen', '');
+  iframe.setAttribute('allow', 'autoplay');
+  iframe.setAttribute('src', generateURL(id));
+  iframe.classList.add('video__media');
+
+  return iframe;
+}
+
+function generateURL(id: string) {
+  let query = '?rel=0&showinfo=0&autoplay=1';
+
+  return 'https://www.youtube.com/embed/' + id + query;
+}
+
+findVideos();
